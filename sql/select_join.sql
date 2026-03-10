39a9e89f32476f87e3923371ab9287e8e6d57cde
Mirjana Ristic <mima34.mg@gmail.com>
2026-03-10 19:11:20 +0100

    Add join queries
    Added SQL queries to join Posts and Users, and Likes with Users and Posts.

--- sql/select_join.sql
+++ sql/select_join.sql
@@ -1,1 +1,13 @@
 -- join queries
+
+-- show posts with the username of the author
+SELECT Posts.Content, Users.Username
+FROM Posts
+JOIN Users ON Posts.UserId = Users.Id;
+
+-- show which user liked which post
+SELECT Users.Username, Posts.Content
+FROM Likes
+JOIN Users ON Likes.UserId = Users.Id
+JOIN Posts ON Likes.PostId = Posts.Id;
+
