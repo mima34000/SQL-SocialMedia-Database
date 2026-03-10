6e44b7742e0b35d43febaf9833c090708f972e7c
Mirjana Ristic <mima34.mg@gmail.com>
2026-03-10 19:01:41 +0100

    Add basic select queries

--- sql/select_basic.sql
+++ sql/select_basic.sql
@@ -1,1 +1,26 @@
 -- basic select queries
+
+-- show all users
+SELECT * FROM Users;
+
+-- find posts written by user with id 1
+SELECT * FROM Posts
+WHERE UserId = 1;
+
+-- show users ordered by username
+SELECT * FROM Users
+ORDER BY Username;
+
+-- find posts that contain the word 'Hello'
+SELECT * FROM Posts
+WHERE Content LIKE '%Hello%';
+
+-- count number of posts per user
+SELECT UserId, COUNT(*) AS PostCount
+FROM Posts
+GROUP BY UserId;
+
+-- show likes for post with id 1
+SELECT * FROM Likes
+WHERE PostId = 1;
+
