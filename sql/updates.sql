89df8b905b085a66dd6acf3a4f4e1b70182db486
Mirjana Ristic <mima34.mg@gmail.com>
2026-03-10 19:15:56 +0100

    Add update queries for user email and post content

--- sql/updates.sql
+++ sql/updates.sql
@@ -1,1 +1,12 @@
 -- update queries
+
+-- update email for user with id 1
+UPDATE Users
+SET Email = 'anna_new@email.com'
+WHERE Id = 1;
+
+-- update content of a post
+UPDATE Posts
+SET Content = 'Learning SQL is fun'
+WHERE Id = 2;
+
