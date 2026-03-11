6d0fe0143a25f7f943fda3cf47ba78400a7c55ae
Mirjana Ristic <mima34.mg@gmail.com>
2026-03-11 01:45:47 +0100

    Modify post content to include databases
    Updated the content of a post to reflect a broader topic.

--- sql/updates.sql
+++ sql/updates.sql
@@ -1,12 +1,4 @@
--- update queries
-
--- update email for user with id 1
-UPDATE Users
-SET Email = 'anna_new@email.com'
-WHERE Id = 1;
-
--- update content of a post
 UPDATE Posts
-SET Content = 'Learning SQL is fun'
+SET Content = 'Learning SQL and databases'
 WHERE Id = 2;
 
